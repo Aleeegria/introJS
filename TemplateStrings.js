6f1c8aa4f8a158b82471fc54c41fd336499caf89
Alegria Alexa <alepope66@gmail.com>
2023-02-05 12:00:57 -0300

    "exercicios"

--- TemplateStrings.js
+++ TemplateStrings.js
@@ -22,7 +22,16 @@ const meuNome1 = "Ale";
 const idade1 = 2023 - 1966;
 const naturalidade1 = "Brasília";
 
-const apresentacao2 = `Meu nome é $(meuNome1), minha idade é $
-(idade1) e nasci na cidade de $(naturalidade1)`;
+const apresentacao2 = `Meu nome é ${meuNome1}, minha idade é 
+${idade1} e nasci na cidade de ${naturalidade1}`;
 
 console.log(apresentacao2);
+
+//exemplo aula
+const nome1 = "Leo";
+const idade2 = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome1} diz: "por favor, quero beber ${idade2 >= 18 ? bebidaMaior : bebidaMenor}"`;
+console.log(pedido);
