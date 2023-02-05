0315c65dfca57878516498f6f7fd874b79d027a8
Alegria Alexa <alepope66@gmail.com>
2023-02-05 11:18:38 -0300

    "exercicios"

--- OperTernario.js
+++ OperTernario.js
@@ -3,7 +3,18 @@
 // if_then_else reduzido
 
 const idadeMinima = 18;
-const idadeCliente = 16;
+const idadeCliente = 19;
+
+if (idadeCliente >= idadeMinima){
+    console.log("Cerveja !!!")
+} else {
+    console.log("Suco !!!")
+}
+
+// operador ternario p/ comparações curtas
+// ternário -> 3 comparadores = ( >=, ?, :)
+console.log(idadeCliente >= idadeMinima ? "Cerveja!" : "Suco!!!");
+
 
 
 
