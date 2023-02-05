508e99c03440886d1d3b0ced7324c4e0985b6c5b
Alegria Alexa <alepope66@gmail.com>
2023-02-05 10:06:32 -0300

    "exercicios"

---
+++ consoleLog.js
@@ -0,0 +1,24 @@
+// uso do console -> saída padrão, o terminal
+// log() = método com registro com informação
+// mostra no terminal o que está entre parenteses
+// 
+
+const minhaVar = true;
+
+console.log(234);
+console.log("Eu sou um texto");
+console.log(minhaVar);
+
+//stacktrace -> rastrear erro
+
+// tratamento de erro
+console.error("Putz !!! Deu ERRO !!!!")
+
+//exemplo aula
+console.log("deu erro");
+console.error("deu erro");
+
+//outro exemplo aula
+console.log("deu erro");
+console.error(new Error("deu erro")); // com new
+// é uma classe em JS. Mostra os errors.
