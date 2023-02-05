8ddba8fb34579651acd2bf1684d67533b32bb9a1
Alegria Alexa <alepope66@gmail.com>
2023-02-05 15:32:20 -0300

    "exercicios"

---
+++ adicionarNotas.js
@@ -0,0 +1,7 @@
+// adicionar elementos no array
+const notas = [10, 6, 8];
+notas.push(7); // acrescenta eçemento no array
+console.log(notas);
+
+const media = (notas[0] + notas[1] + notas[2]+ notas[3]) / notas.length;
+console.log(media);
