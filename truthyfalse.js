0f7ae8246850fc6dd8eea4751c6202e3e91abb0f
Alegria Alexa <alepope66@gmail.com>
2023-02-05 08:18:16 -0300

    "truthyfalse"

--- truthyfalse.js
+++ truthyfalse.js
@@ -7,4 +7,23 @@ const contaPaga = false;
 
 console.log(0 == false) // compara valor // === compara tipo e valor
 console.log("" == false)
+console.log(1 == true)
+
+// null -> vazio ou nada
+// undefined -> 
+
+let minhaVar; //undefined
+let varNull = null; //null
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 4;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
 
