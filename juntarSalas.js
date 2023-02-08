7ae9e635c91145ba8d1a6fb342a28fddee1c001b
Alegria Alexa <alepope66@gmail.com>
2023-02-08 15:25:47 -0300

    "exercicios"

---
+++ juntarSalas.js
@@ -0,0 +1,8 @@
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+const salasUnificadas = salaJS.concat(salaPython);
+const salasUnificadas1 = salaPython.concat(salaJS);
+
+console.log(salasUnificadas);
+console.log(salasUnificadas1);
