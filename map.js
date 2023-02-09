d6331b66b62f71ec5ad675de36d9e0268e7f2f06
Alegria Alexa <alepope66@gmail.com>
2023-02-09 15:29:43 -0300

    "Exercicios"

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
 const notas = [10, 9.5, 8, 7, 6];
 
 const notasAtualizadas = notas.map((nota) => {
-    return nota + 1 >= 10 ? 10 : nota + 1;
+    return nota + 1 >= 10 ? 10 : nota + 1; //operador ternario
+       // nota + 1 é igual ou maior a 10? se sim, 10, senão nota+1               
 });
 
 console.log(notasAtualizadas);
