7c712e9a47d14679f58f72e515ef8ca8fa31576d
Alegria Alexa <alepope66@gmail.com>
2023-02-09 17:12:22 -0300

    "exercicios"

---
+++ removerDuplicado.js
@@ -0,0 +1,11 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const meuSet = new Set(nomes); // set não repete valores
+
+const nomesAtualizados = [...meuSet];
+
+console.log(meuSet);
+console.log(nomesAtualizados);
+
+
+
