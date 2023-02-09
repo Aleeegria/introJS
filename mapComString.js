797ddba7a3d49598f14e44e27d8701f21db37e1b
Alegria Alexa <alepope66@gmail.com>
2023-02-09 15:48:36 -0300

    "exercicios"

--- mapComString.js
+++ mapComString.js
@@ -3,3 +3,26 @@ const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
 const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
 
 console.log(nomesPadronizados);
+
+//exemplo da aula
+const nome = "Alura";
+let nomeMaiusculas = "";
+
+for (let i = 0; i < nome.length; i++) {
+ nomeMaiusculas += nome[i].toUpperCase()
+}
+console.log(nomeMaiusculas) //ALURA
+
+//outro exemplo
+const nomedoCurso = "Fundamentos de JS";
+const nomeDaPlataforma = " Alura"
+
+const nomeCompleto = nomedoCurso.concat(nomeDaPlataforma)
+console.log(nomeCompleto)  //Fundamentos de JS Alura
+
+//outro exemplo
+const arrayNums = [1, 2, 3, 4]
+
+const arraySomada = arrayNums.map(num => num * 10)
+
+console.log(arraySomada)
