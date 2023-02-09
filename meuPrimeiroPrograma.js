36934c044f3b48c4707ffe5bafb8e893d3fcfb8b
Alegria Alexa <alepope66@gmail.com>
2023-02-09 18:00:43 -0300

    "exercicios"

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+console.log("Meu primeiro Programa: trabalhando com variáveis !!!");
+
+const idade = 56;
+
+console.log(idade);
+console.log(idade + 2);
+console.log(idade / 2);
+console.log(idade - 2);
+console.log(idade * 2);
+
+const idadeSomada = idade + 3;
+console.log(idadeSomada);
+
+
+
