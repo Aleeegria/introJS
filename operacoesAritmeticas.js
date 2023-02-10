d8fc17ac33849feb995d2232c7a108c89466823a
Alegria Alexa <alepope66@gmail.com>
2023-02-10 15:34:13 -0300

    "exercicios"

---
+++ operacoesAritmeticas.js
@@ -0,0 +1,14 @@
+console.log("Operações Aritméticas e Conversão de Tipos !!!")
+
+console.log(2+2);
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+console.log(10 + (8 * 2));
+
+console.log("ANO " + 2023);
+console.log("2" + "2");//concatenção apenas
+console.log(parseInt("2") + parseInt("2"));//conversão explicita
+console.log("10" / "5");//conversão implicita
+
+
+
