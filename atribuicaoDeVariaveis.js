94285d39d5102df11d002e404923fd3a038e1ee3
Alegria Alexa <alepope66@gmail.com>
2023-02-10 16:16:03 -0300

    "exercicios"

---
+++ atribuicaoDeVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Atribuição de variáveis");
+
+const idade = 56;
+const nome = "Ale ";
+const sobrenome = "Potter";
+
+console.log(nome + sobrenome);
+console.log(nome, sobrenome);
+console.log(nome + " " + sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
