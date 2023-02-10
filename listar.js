479cf14aacdbda5ab1cfc87e05631790375798aa
Alegria Alexa <alepope66@gmail.com>
2023-02-10 16:45:03 -0300

    "exercicios"

---
+++ listar.js
@@ -0,0 +1,20 @@
+console.log("Trabalhando com listas !!!!")
+
+// CTL + K + C = comenta em bloco
+// CTL + K + U = descomenta em bloco
+
+// const salvador = 'Salvador';
+// const saoPaulo = 'São Paulo';
+// const rioDeJaneiro = 'Rio de Janeiro';
+
+const listaDeDestinos = new Array(
+    'Salvador', 'São Paulo', 'Rio de Janeiro', 'Curitiba'
+);
+
+listaDeDestinos.push('Recife');//adicionar item na lista
+
+console.log("Destinos possiveis : ");
+//console.log(salvador, saoPaulo, rioDeJaneiro);
+
+console.log(listaDeDestinos);
+
