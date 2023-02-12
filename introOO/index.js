b67ce7511b239641674d55ff5302ed674ccda37e
Alegria Alexa <alepope66@gmail.com>
2023-02-12 11:05:09 -0300

    "exercicios"

--- introOO/index.js
+++ introOO/index.js
@@ -2,18 +2,18 @@ import {Cliente} from "./Cliente.js"
 import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente("Alê", 12345678912);
-conta1.agencia = 1001;
+const conta1 = new ContaCorrente(1001, cliente1);
 conta1.depositar(5000);
 
-const cliente2 = new Cliente("Pedro", 23456789123 );
-conta2.agencia = 1002;
+const cliente2 = new Cliente("Pedro", 23456789123);
+const conta2 = new ContaCorrente(1002, cliente2);
 
 const cliente3 = new Cliente("Augusto", 34567891234);
-conta3.agencia = 1003;
+const conta3 = new ContaCorrente(1003, cliente3);
 
 let valor = 1000;
-conta1.transferir(valor, conta2);
-conta1.transferir(valor, conta3);
+cliente1.transferir(valor, conta2);
+cliente1.transferir(valor, conta3);
 console.log("valor: ", valor);
 console.log(cliente1);
 console.log(cliente2);
