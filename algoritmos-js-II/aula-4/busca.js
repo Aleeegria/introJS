b7c2e92d11a0b10e241448125c3462da749baabd
Alegria Alexa <alepope66@gmail.com>
2023-02-18 16:52:06 -0300

    "exercicios"

---
+++ algoritmos-js-II/aula-4/busca.js
@@ -0,0 +1,28 @@
+console.log('Busca binária divide a lista de busca em 2 partes, procura em uma das duas' 
++ ', depois descarta a lista onde não foi comparado, depos divide em 2 até achar. ');
+
+const listaLivros = require('./arrayOrdenado');
+
+function busca(array, de, ate, valorBuscado) {    
+
+    const meio = Math.floor((de + ate) / 2);
+    const atual = array[meio];
+
+    if(de > ate) {
+        return -1;
+    }
+
+    if(valorBuscado === atual.preco) {
+        return meio;
+    }
+
+    if(valorBuscado < atual.preco) {
+        return busca(array, de, meio - 1, valorBuscado);
+    }
+
+    if(valorBuscado > atual.preco) {
+        return busca(array, meio + 1, ate, valorBuscado);
+    }
+}
+
+console.log(busca(listaLivros, 0, listaLivros.length -1, 40));
